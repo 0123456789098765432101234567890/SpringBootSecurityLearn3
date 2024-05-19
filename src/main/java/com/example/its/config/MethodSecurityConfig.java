1b0b764a28d157217d56f7e8afbc5396f9f97b59
K <smghank@icloud.com>
2024-05-19 14:38:40 +0900

    main

---
+++ src/main/java/com/example/its/config/MethodSecurityConfig.java
@@ -0,0 +1,7 @@
+package com.example.its.config;
+
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+
+@EnableMethodSecurity
+public class MethodSecurityConfig {
+}
---
+++ src/test/java/com/example/demo/SpringBootSecurityLearn3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootSecurityLearn3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
